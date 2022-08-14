2df6f4b9fdffa9131e09f907ec99c6d0f9205404
Buzz <jure.eres42@gmail.com>
2022-08-14 14:46:46 +0200

    0.0.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
 
-function App() {
-  return (
-    <div className="App">
+/*
+Challenge:
 
-    </div>
-  );
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar
+  (in place of the current <Card /> component)
+
+Note: We haven't styled the group of components yet, so they'll
+still be block elements, stacked vertically. We'll add styling later.
+*/
+
+export default function App() {
+            // <Hero />
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <Card 
+                img="katie-zaferes.png"
+                rating="5.0"
+                reviewCount={6}
+                location="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+            />
+        </div>
+    )
 }
-
-export default App;
